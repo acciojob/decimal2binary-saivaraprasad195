b0ae26d798c154fbdec947c3532e63886d3230fa
saivaraprasad195 <62549091+saivaraprasad195@users.noreply.github.com>
2024-01-18 22:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let bin =0;
+	while(num>0){
+		bin= (num%2).toString + "";
+		num/=2; 
+	}
 }
 
 window.decimalToBinary = decimalToBinary;
